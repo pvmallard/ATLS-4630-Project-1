62697eb141b09abadd0e89729381dd55de24093f
Mallory Prescott <mvprescott17@gmail.com>
2024-10-04 13:17:26 -0600

    Set up Library

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -1,7 +1,5 @@
-import { gsap } from "gsap";
-    
+import { gsap } from "gsap"; 
 import { RoughEase, ExpoScaleEase, SlowMo } from "gsap/EasePack";
-    
 import { Flip } from "gsap/Flip";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { Observer } from "gsap/Observer";
@@ -12,10 +10,31 @@ import { EaselPlugin } from "gsap/EaselPlugin";
 import { PixiPlugin } from "gsap/PixiPlugin";
 import { TextPlugin } from "gsap/TextPlugin";
 
+console.log(gsap);
 
 gsap.registerPlugin(Flip,ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin,EaselPlugin,PixiPlugin,TextPlugin,RoughEase,ExpoScaleEase,SlowMo);
 
+let tl = 
+    gsap.timeline(), 
+    mySplitText = new SplitText("#name", { type: "words,chars"}), 
+    chars = mySplitText.chars;
+gsap.set("#name", {perspective: 400});
+console.log(chars);
 
+tl.from(chars, {
+    duration: 0.8,
+    opacity: 0,
+    scale: 0,
+    y: 80,
+    rotationX: 180,
+    transformOrigin: "0% 50% -50",
+    ease: "back",
+    stagger: 0.01
+  });
+
+// document.getElementById("animate").onclick = function () {
+//     tl.restart();
+// };
 
 // fetch('https://allergymenu.uk/api/v1') // fetch API
 //     .then(response => console.log(response.json()))
